c0d65972a09c9d6e36c508226380497dae1ba302
Patrycja <patrysia1912@gmail.com>
2026-06-03 12:24:16 +0200

    Fix issues with prints

--- customer.py
+++ customer.py
@@ -6,7 +6,7 @@ class Customer:
         self.name = name
 
     def order_transport(self, service):
-        print(f"{self.name} order transport {service.transport_name()}")
+        print(f"\n{self.name} order transport {service.transport_name()}")
         return service.order_transport()
 
 
