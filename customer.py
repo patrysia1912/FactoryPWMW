be4e0ad5e2b6319481cf91d378671fa7626e2455
Patrycja <patrysia1912@gmail.com>
2026-06-03 11:50:33 +0200

    Add printing of ordering, how much it will take for vehicle to arrive and how long the drive will take

--- customer.py
+++ customer.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
 
+
 class Customer:
     def __init__(self, name):
         self.name = name
 
     def order_transport(self, service):
-        print(f"{self.name} order transport {service}")
-        service.order_transport()
+        print(f"{self.name} order transport {service.transport_name()}")
+        return service.order_transport()
 
 
